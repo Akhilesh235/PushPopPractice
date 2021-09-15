b6ba6853e156306330ccaf7526469f80876c5651
Akhilesh235 <90238925+Akhilesh235@users.noreply.github.com>
2021-09-15 17:49:04 +0800

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PushPopPractice1
+{
+    public class Element
+    {
+        public int Value
+        {
+            get; set;
+        }
+
+        public void displayValue()
+        {
+            Console.WriteLine("I am outside the assembly" + Value);
+        }
+    }
+}
---
+++ PushPopPractice/Program.cs
@@ -0,0 +1,45 @@
+﻿
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Console = System.Console;
+
+namespace PushPopPractice
+{
+    public class Stack
+    {
+        static void Main(string[] args)
+        {
+            // Car c = new Car();
+            Element temp = new Element();
+
+            temp.displayValue();
+            //Stack<string> stack = new Stack<string>();
+            //stack.Push("A");
+            //stack.Push("B");
+            //stack.Push("C");
+            //stack.Push("D");
+
+            //foreach (string stackk in stack)
+
+            //{
+            //    Console.WriteLine(stackk);
+            //}
+
+            //Console.WriteLine("Pop the first");
+            //stack.Pop();
+            //foreach (string pop in stack)
+            //{
+            //    Console.WriteLine(pop);
+            //}
+
+            //Console.WriteLine("Peek");
+            //Console.WriteLine(stack.Peek());
+
+            Console.ReadLine();
+
+        }
+    }
+}
